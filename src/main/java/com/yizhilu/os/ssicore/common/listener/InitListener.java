3e4b9995bd8fd10a24fd97b7858bd9b796b22ca9
fairyhawk <bis@foxmail.com>
2014-11-03 09:31:25 +0800

    不拦截127. 192.
    不拦截127. 192.

--- src/main/java/com/yizhilu/os/ssicore/common/listener/InitListener.java
+++ src/main/java/com/yizhilu/os/ssicore/common/listener/InitListener.java
@@ -1,12 +1,12 @@
 package com.yizhilu.os.ssicore.common.listener;
 
+import com.yizhilu.os.ssicore.util.PropertyUtil;
+import org.springframework.web.context.ContextLoaderListener;
+
+import javax.servlet.ServletContextEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-
-import javax.servlet.ServletContextEvent;
-
-import org.springframework.web.context.ContextLoaderListener;
 
 /**
  * @ClassName com.yizhilu.os.ssicore.common.InitListener
@@ -20,6 +20,15 @@ public class InitListener extends ContextLoaderListener {
     }
 
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        try{
+            PropertyUtil propertyUtil = PropertyUtil.getInstance("project");
+            String contextPath = propertyUtil.getProperty("contextPath");
+            if(contextPath.indexOf("127.0.")>0 ||contextPath.indexOf("192.168.")>0  ){
+                super.contextInitialized(servletContextEvent);
+                return;
+            }
+        }catch (Exception e1){
+        }
         try {
             URL url = new URL("http://jk.268xue.com:8888/company/checkCompany.268");
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
